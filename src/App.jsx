303c94c53842e28ad59427e07173310a5a5dcd15
Tofiq311 <ashik.tofiq.1@gmail.com>
2024-08-23 19:58:58 +0900

    my new commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RandomQuote from './Components/RandomQuote/RandomQuote'
+
+const App = () => {
+  return (
+    <div>
+      <RandomQuote/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './RandomQuote.css'
+import twitter_icon from '../../assets/twitter-x.png'
+import reload_icon from '../../assets/reload.png'
+import facebook_icon from '../../assets/facebook.png'
+
+const RandomQuote = () => {
+
+    let quotes = [];
+
+    async function loadQuotes() {
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random()*quotes.length)];
+        setQuote(select);
+    }
+
+    const [quote,setQuote] = useState({
+        text: "Difficulties increase the never we get",
+        author: "Joen Doe",
+
+     });
+
+     const twitter = () => {
+        window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(',')[0]}`)
+     }
+
+     const facebook = () => {
+        window.open(`https://facebook.com/feed?text=${quote.text} - ${quote.author.split(',')[0]}`)
+     }
+
+     loadQuotes();
+
+  return (
+    <div className='container'>
+        <div className='quote'>{quote.text}</div>
+        <div>
+            <div className='line'></div>
+            <div className='bottom'>
+             <div className='author'>- {quote.author.split(',')[0]}</div>
+             <div className='icons'>
+                <img src={reload_icon} onClick={()=>{random()}} alt='' />
+                <img src={twitter_icon} onClick={()=>{twitter()}} alt='' />
+                <img src={facebook_icon} onClick={()=>{facebook()}} alt='' />
+             </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default RandomQuote
